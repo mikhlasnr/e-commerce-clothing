a4262859cd2d1221bcc87d767cc338faa27f3b4e
mikhlasnr <minrnullpo@gmail.com>
2023-03-21 22:28:39 +0700

    implement styled component to main container on progress

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -10,38 +10,36 @@ import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
-import "./navigation.styles.scss";
-
+import {
+  NavigationContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink,
+} from "./navigation.styles";
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
-      <div className="navigation">
-        <div className="container-main container--navigation">
-          <Link className="logo-container" to="/">
-            <CrwnLogo className="logo" />
-          </Link>
-          <div className="nav-links-container">
-            <Link className="nav-link" to="/shop">
-              SHOP
-            </Link>
+      <NavigationContainer>
+        <LogoContainer to="/">
+          <CrwnLogo className="logo" />
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to="/shop">SHOP</NavLink>
 
-            {currentUser ? (
-              <span className="nav-link" onClick={signOutUser}>
-                SIGN OUT
-              </span>
-            ) : (
-              <Link className="nav-link" to="/auth">
-                SIGN IN
-              </Link>
-            )}
-            <CartIcon />
-          </div>
-          {isCartOpen && <CartDropdown />}
-        </div>
-      </div>
+          {currentUser ? (
+            <NavLink as="span" onClick={signOutUser}>
+              SIGN OUT
+            </NavLink>
+          ) : (
+            <NavLink to="/auth">SIGN IN</NavLink>
+          )}
+          <CartIcon />
+        </NavLinks>
+        {isCartOpen && <CartDropdown />}
+      </NavigationContainer>
       <Outlet />
     </Fragment>
   );
