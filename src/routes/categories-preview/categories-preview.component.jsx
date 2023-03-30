30e45dd9d5a2f36e04f038e8f4aefd17252f0ad4
mikhlasnr <minrnullpo@gmail.com>
2023-03-31 00:33:19 +0700

    implement all styles with styled component

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,7 +1,7 @@
-import { useContext, Fragment } from 'react';
+import { useContext, Fragment } from "react";
 
-import { CategoriesContext } from '../../contexts/categories.context';
-import CategoryPreview from '../../components/category-preview/category-preview.component';
+import { CategoriesContext } from "../../contexts/categories.context";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 const CategoriesPreview = () => {
   const { categoriesMap } = useContext(CategoriesContext);
@@ -9,9 +9,12 @@ const CategoriesPreview = () => {
   return (
     <Fragment>
       {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
         return (
-          <CategoryPreview key={title} title={title} products={products} />
+          <CategoryPreview
+            key={title}
+            title={title}
+            products={categoriesMap[title]}
+          />
         );
       })}
     </Fragment>
